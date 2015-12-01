71974da081212e6a03d431a74026cd9a2165523f
Mathieu Fannee <mathieu.fannee@inl.nl>
2015-12-01 17:26:00 +0100

    Lex'it commit some modified or new project config files

--- WebContent/wntlex_dev.config.js
+++ WebContent/wntlex_dev.config.js
@@ -5,6 +5,75 @@ oShowOnlyTables = [];
 // table general settings
 oTableSettingsList = {
 		
+// special job!!!
+		
+		new_mnw_wnt_links_view:{
+			
+			callback: function(t){
+				
+				putSearchInterface();
+				
+			},
+			
+			"button_0": {
+				
+				"name": "(Un)linken",
+				"click": function(t){
+					
+					var n = fn.getFirstSelectedRowFrom(t);
+					var sMnwInTable = fn.getDataFromCellNamed(t, n, "mnw_persistent_id");
+					var sWntInTable = fn.getDataFromCellNamed(t, n, "wnt_persistent_id");
+					
+					var sNewIdContent = $("#id_query").val();
+					var sIdType = (sNewIdContent.match(/^[MAS]\d+/)) ? "WNT" : "MNW";
+					
+					if (sMnwInTable!= '' && sWntInTable != '') // we have a link, remove it
+						{
+						fn.removeFromDatabaseGivenFieldValues("new_mnw_wnt_links", 
+								{
+								"mnw_persistent_id": sMnwInTable,
+								"wnt_persistent_id": sWntInTable
+								}, false,
+								function(){fn.refreshTable(t);});
+						}
+					else if (sMnwInTable!= ''      // mnw id chosen in table
+						&& sIdType == 'WNT'        // wnt id chose in dictionary part
+						&& sNewIdContent != '')
+						{
+						fn.insertIntoDatabase("new_mnw_wnt_links", 
+								{
+								"mnw_persistent_id": sMnwInTable,
+								"wnt_persistent_id": sNewIdContent
+								}, null, false,
+								function(){fn.refreshTable(t);});
+						}
+					else if (sWntInTable != ''      // wnt id chosen in table
+						&& sIdType == 'MNW'         // mnw id chose in dictionary part
+						&& sNewIdContent != '')
+						{
+						fn.insertIntoDatabase("new_mnw_wnt_links", 
+								{
+								"mnw_persistent_id": sNewIdContent,
+								"wnt_persistent_id": sWntInTable
+								}, null, false,
+								function(){fn.refreshTable(t);});
+						}
+					else {
+						
+						fn.message("Werkwijze", "Kies een regel in de tabel die een MNW- of WNT-id heeft," +
+								"en kies in het GBT-gedeelte een corresponderend MNW- of WNT-lemma," +
+								"en klik dan pas op 'Linken'");
+					}
+						
+				}
+				
+			}
+			
+		},
+		
+		
+
+		
 		token_attestations: {
 			
 			callback: function(t){
@@ -44,7 +113,8 @@ oTableSettingsList = {
 						// to another lemma 
 						
 						fn.confirm("Let op", 
-								"De geselecteerde attestaties zullen gekoppeld worden aan lemma '"+sLemma+"' " +
+								"De geselecteerde attestaties zullen gekoppeld worden aan " +
+								(sLemId != '' ? "" : "multiple ") + "lemma '"+sLemma+"' " +
 								"met ID " + (sLemId != '' ? sLemId : sMultiLemId) + ".<br>" +
 								"Weet u zeker dat u dat wilt?", function(){
 							
@@ -108,6 +178,8 @@ oTableSettingsList = {
 																	
 																	
 																	// clean up paradigm and refresh it
+																	// t.i. remove the original analyzed_wordforms 
+																	// if there remained no attestation refering to it
 																	aOriginalAwfIdsAndGroupId = getOnlyUniqueValues(aOriginalAwfIdsAndGroupId);
 																	for (var i=0; i<aOriginalAwfIdsAndGroupId.length; i++)
 																		{
@@ -144,6 +216,30 @@ oTableSettingsList = {
 					
 				}
 				
+			},
+			
+			
+			"button_1":{
+				
+				"name": "Schoonvegen",
+				"tooltip": "Verwijder alle attestaties uit de citaat",
+				"click": function(t){
+					
+					fn.confirm("Citaat schoonvegen", "Weet u het zeker?", function(){
+						
+						var n = fn.getFirstSelectedRowFrom(t);
+						
+						// remove indexes
+						fn.updateDatabaseGivenANode(
+								t, 
+								n, 
+								[ "onsetoffset" ], 
+								[ "none" ], 
+								true);
+						
+					});					
+				}
+				
 			}
 			
 		},
@@ -159,6 +255,12 @@ oTableSettingsList = {
 						var aRows = fn.getSelectedRowsFrom(t);
 						aRows.each(function(){
 							var n = this;
+							
+							// This function doesn't try to assign a new lemma_id
+							// to prevent conflicting ids, as we assume that ids of newly
+							// added lemmata come on top of the existing id-values.
+							// So when some lemma is removed, its id won't be re-used
+							// so it is safe to reload the old lemma with its original id.
 							
 							var sLemId = fn.getDataFromCellNamed(t, n, "lemma_id");
 							fn.callFunction("api.restore_lemma", [sLemId], function(){
@@ -188,6 +290,12 @@ oTableSettingsList = {
 						var aRows = fn.getSelectedRowsFrom(t);
 						aRows.each(function(){
 							var n = this;
+							
+							// This function doesn't try to assign a new analyzed_wordform_id
+							// to prevent conflicting ids, as we assume that ids of newly
+							// added analyzed_wordforms come on top of the existing id-values.
+							// So when some analyzed_wordform is removed, its id won't be re-used
+							// so it is safe to reload the old analyzed_wordform with its original id.
 							
 							var sAwfId = fn.getDataFromCellNamed(t, n, "analyzed_wordform_id");
 							fn.callFunction("api.restore_paradigm", [sAwfId], function(){
@@ -230,6 +338,9 @@ oTableSettingsList = {
 										[sLemma, sPos, sPersistentId], 
 										function(){
 									
+											// the lemma was created in the lemmata table 
+											// and copied by a trigger to the current table, so make
+											// the result visible
 											fn.refreshTable(t);
 									
 								});
@@ -356,27 +467,35 @@ oTableConfigurationList = {
 				}
 		},
 		
-		bronnen_worktable:{
-			
-			unique_id: {
-				"colsort": "asc",
-				"visible": false
-			},
-			van: {
-				"bgcolor": "#E0F8EC",
-				"editable": true
-			},
-			tot: {
-				"bgcolor": "#E0F8EC",
-				"editable": true
-			}
-		},
+//		bronnen_worktable:{
+//			
+//			unique_id: {
+//				"colsort": "asc",
+//				"visible": false
+//			},
+//			van: {
+//				"bgcolor": "#E0F8EC",
+//				"editable": true
+//			},
+//			tot: {
+//				"bgcolor": "#E0F8EC",
+//				"editable": true
+//			}
+//		},
 
 		
 		lemmata_and_paradigma: {
 			
+			super_lem_id :{
+				"colsort": "asc" // sort #1
+			},
+			
+			wdb: {
+				"choosefrom": []
+			},
+			
 			modern_lemma: {
-				"colsort": "asc", // sort #1
+				"colsort": "asc", // sort #2
 				"editable": true,
 				"editfunc": function(t, n, value){
 					
@@ -391,12 +510,13 @@ oTableConfigurationList = {
 				}
 			},
 			persistent_id: {
-				"colsort": "asc", // sort #2
-				"cell_tooltip": "Open WNT",
+				"colsort": "asc", // sort #3
+				"cell_tooltip": "Open WDB",
 				"click": function(t,n){
 					
+					var sWdb = fn.getDataFromSiblingNode(t, n, "wdb");
 					var iPersistentId = fn.getDataFromCellNode(t, n);
-					window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb=WNT&id="+iPersistentId);
+					window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb="+sWdb+"&id="+iPersistentId);
 					
 				}
 			},
@@ -422,7 +542,7 @@ oTableConfigurationList = {
 				}
 			},
 			group_id: {
-				"colsort": "asc", // sort #3
+				"colsort": "asc", // sort #4
 				"cell_tooltip": "Totaal citaten bijbehorend bij groep",
 				"click": function(t,n){
 					
@@ -572,7 +692,7 @@ oTableConfigurationList = {
 							// we need to rebuild the other fields content accordingly:
 												
 							// [1] in a first step we gather the highlight wordforms
-							//     and put those in the same order als the their onsets (=keep relation!) 
+							//     and put those in the same order as the their onsets (=keep relation!) 
 													
 							computeWordsFromIndexes(t, n);
 															
@@ -634,14 +754,14 @@ oTableConfigurationList = {
 																	
 																			// refresh to see the results!
 																			fn.refreshTable("lemmata_and_paradigma");
-																	fn.refreshTable(t);
+																			fn.refreshTable(t);
 																	
 																});
 																
 												});
 										
 									
-												});
+											});
 								});
 								
 								
@@ -740,13 +860,91 @@ oTableConfigurationList = {
 						}					
 				}
 			}
+		},
+		
+		
+		
+		// special job!!!
+		
+		new_mnw_wnt_links_view:{
+			
+			mnwlem:{
+				"colsort": "asc"   // sort #1
+					},
+			wntlem:{
+				"colsort": "asc"   // sort #2
+			},
+			mnw_persistent_id: {
+				"cell_tooltip": "Klik om op te zoeken",
+				"click": function(t, n){
+					
+					var iPersistentId = fn.getDataFromCellNode(t, n);
+					window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb=MWN&id="+iPersistentId);
+					window.open("http://gtb.dev.inl.loc/lexit/?db=wntlex_dev&table=lemmata_and_paradigma&persistent_id="+iPersistentId);
+						
+				}
+			},
+			
+			wnt_persistent_id: {
+				"cell_tooltip": "Klik om op te zoeken",
+				"click": function(t, n){
+					
+					var iPersistentId = fn.getDataFromCellNode(t, n);
+					window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb=WNT&id="+iPersistentId);
+					window.open("http://gtb.dev.inl.loc/lexit/?db=wntlex_dev&table=lemmata_and_paradigma&persistent_id="+iPersistentId);
+						
+				}
+			}
+			
+		}
+		
+		,		
+		groups_table_unique: {
+			
+			array_length: {
+				"colsort": "desc"
+			},
+			
+			group_arr:{
+				
+				"click": function(t, n){
+					
+					// do we have a text selection?
+					var oSelectedText = fn.getSelectedTextInNode(t, n);
+					
+					// if selection is empty, that means that we've clicked on a word
+					// without selecting it manually.
+					// In this case, try to select the word that was clicked upon
+					if (oSelectedText.text == '' && oSelectedText.reliable)
+						{						
+						oSelectedText = fn.getWordClickedUponInNode(t, n);						
+						}
+					
+					// if we have a selection now, process it
+					if (oSelectedText.text!='' && oSelectedText.reliable)
+						{
+						var selectedId = oSelectedText.text;
+						selectedId = selectedId.replace('{','');
+						selectedId = selectedId.replace('}','');
+						
+						if (selectedId.match(/^[ASM]/))
+							{
+							window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb=WNT&id="+selectedId);
+							}
+						else
+							window.open("http://gtb.inl.nl/iWDB/search?actie=article&wdb=MNW&id="+selectedId);
+						}
+				}
+				
+			}
+			
 		}
 };
 
 
 // start with the main table
 
-fn.callDatabase("lemmata_and_paradigma");
+//fn.callDatabase("lemmata_and_paradigma");
 
 
 
@@ -835,7 +1033,7 @@ function getArgumentsForFindingOrCreatingAnalyzedWordform(t, n){
 // needed functions for highlight
 // (2 versions available)
 
-// highlight in 'token_attestations' table
+// [1] highlight in 'token_attestations' table
 
 function _highlightAllQuotes(confTable){
 	
@@ -873,7 +1071,7 @@ function _putHighlightOnOneRow(confTable, confNode) {
 };
 
 
-// highlight in 'token_attestations_worktable' table
+// [2] highlight in 'token_attestations_worktable' table
 
 function highlightAllQuotes(confTable){
 	
@@ -1013,3 +1211,315 @@ function highlightGroups(t){
 	
 };
 
+
+
+
+
+// special job
+
+function search(){
+	
+	
+	var idString = $('#id_query').val();
+	var wordString = $('#word_query').val();
+	var oldwordString = $('#oldword_query').val();
+	
+	if (wordString != '')
+		{
+		GetGTBlist(wordString, false);
+		}
+	else if (oldwordString != '')
+		{
+		GetGTBlist(oldwordString, true);
+		}
+	else if (idString != '')
+		{
+		OpenRightGTBpage(idString);
+		}
+}
+
+
+function putSearchInterface(){
+	
+	$("#new_mnw_wnt_links_view_dynamic").append(
+			$("<div></div>").html(
+					'<form id="gtb_form" name="gtb_form" onsubmit="search();return false">'+
+					'ID: <input title="Tik het GTB ID in dat u wilt opzoeken (gebruik wel ONW of VMNW als prefix voor deze 2 woordenboeken; niet nodig voor MNW of WNT)" type="text" id="id_query" name="id_query">'+					
+					'Modern lemma: <input type="text" title="Tik het moderne lemma in dat u wilt opzoeken" id="word_query" name="word_query">'+ 
+					'Historisch lemma: <input type="text" title="Tik het historische lemma in dat u wilt opzoeken" id="oldword_query" name="oldword_query">'+
+					'<input type="button" title="Klik hier om te zoeken in de GTB" value="Zoeken" onclick="search()">'+
+					'</form>'		
+			)
+			.css("position", "relative")
+			.css("top", "5px")
+			.css("left", "0px")
+			.attr("id", "gtb_input_fields")
+	);
+	
+	$("#new_mnw_wnt_links_view_dynamic").append(
+			$("<div></div>")
+			.attr("id", "list_of_senses")
+			.css("position", "relative")
+			.css("top", "20px")
+			.css("left", "0px")
+			.css("width", "370px")
+			.css("height", "300px")
+	);
+	
+	$("#new_mnw_wnt_links_view_dynamic").append(
+			$("<div></div>")
+			.attr("id", "gtb_location")
+			.css("position", "relative")
+			.css("top", "-295px")
+			.css("left", "430px")
+	);
+	
+	$("#new_mnw_wnt_links_view_dynamic").append(
+			$("<div></div>")
+			.attr("id", "gtb_content")
+			.css("position", "relative")
+			.css("top", "-280px")
+			.css("left", "430px")
+			.html('<IFRAME id="gtb_windows" style="height: 380px; width: 800px;"></IFRAME>')
+	);
+
+	$(document).on("keydown", function(e){
+		
+		if (e.which == 13 &&
+				($("#id_query").is(":focus") ||
+				 $("#word_query").is(":focus") ||
+				 $("#oldword_query").is(":focus"))
+			)
+		{
+			search();
+		}
+		
+	});
+	
+	$("#id_query").on("click", function(){
+		$("#word_query").val("");
+		$("#oldword_query").val("");
+	});
+	$("#word_query").on("click", function(){
+		$("#id_query").val("");
+		$("#oldword_query").val("");
+	});
+	$("#oldword_query").on("click", function(){
+		$("#id_query").val("");
+		$("#word_query").val("");
+	});
+	
+}
+
+
+//=======================================================================================
+
+function cleanGTBwindow(){
+	
+	$("#id_query").val("");
+	$("#word_query").val("");
+	$("#oldword_query").val("");
+	$("#list_of_senses").empty();
+	OpenUrlInFrame("", "");
+	
+	// NB about emptying search fields #id_query & #word_query
+	// that also helps preventing a new search from starting automatically, because e.g. when having to
+	//  confirm replacement of an id in a cell, the enter-press event is caught too fast.
+	
+}
+
+
+
+	
+
+// =======================================================================================
+	
+// open an article in the GTB, given a dictionary and an article id
+function OpenRightGTBpage(idString){
+	
+	var special_wdb = "";
+	if (idString.match("^VMNWID"))
+		{
+		special_wdb = "VMNW";
+		idString = idString.replace(/^VMNWID/, "ID");
+		}
+	else if (idString.match("^ONWID"))
+		{
+		special_wdb = "ONW";
+		idString = idString.replace(/^ONWID/, "ID");
+		}
+	
+	var wdb = idString.match("^[A-Z].+") ? ( idString.match("^ID") ? special_wdb : "WNT" ) : "MNW";
+	
+	var url = encodeURI("http://gtb.inl.nl/iWDB/search?actie=article&wdb="+wdb+"&id=" + idString);
+	
+	
+	// get the right article
+	if (idString != '-' && idString != '')
+		{
+		OpenUrlInFrame(url, wdb);
+		}
+	else
+		{
+		OpenUrlInFrame("", "");
+		}
+}
+
+
+// opens a URL in a frame.
+// It binds a (anti)scroll event to prevent scrolling
+// of the screen when opening a URL in the frame, since
+// some automatic scroll event in the frame can cause
+// the whole page to scroll and not only the frame.
+// We keep track of the time ellapsed since the last
+// call of this function, so as to know when to UNBIND
+// the (anti)scroll event.
+
+var lastTimeFrameWasAddressed = new Date();
+var timeToWaitBeforeUnbindingScrollEvent = 1000;
+
+
+function OpenUrlInFrame(url, name){	
+	
+	if ( itsBeenAWhileSinceFrameWasAddressed() )
+		$(this).bind('scroll', scrollEvent);
+
+	lastTimeFrameWasAddressed = new Date();
+	
+	setTimeout("PutUrlInFrame('"+url+"', '"+name+"')",300);
+}
+
+function itsBeenAWhileSinceFrameWasAddressed(){
+	
+	return ( (new Date()-lastTimeFrameWasAddressed) > timeToWaitBeforeUnbindingScrollEvent);
+}
+
+function PutUrlInFrame(url, name){
+	
+	$("#gtb_windows").attr("src", url);
+	$("#gtb_location").text( (name!='' ? "Wordt nu getoond: "+name : "") );
+}
+
+function scrollEvent() {
+	//$('html').scrollTop(0); 
+	//$('html').scrollLeft(0); 
+}
+
+
+//=======================================================================================
+
+// look up the sense of a word in the GTB, given a dictionary (list) and a word
+function GetGTBlist(wordString, isAnOldword){
+	var url = "http://gtb.dev.inl.loc/ikea/rest/consult/search";
+	
+	$.ajax(
+			{
+				type: "GET",
+				url: url,
+				data: {
+					"word": wordString, 
+					"wdb": "ONW,VMNW,MNW,WNT", 
+					"isold": isAnOldword,
+					"dummy": getUniqueNumber()
+					},
+				dataType: "xml",
+				contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+				success: function(xml) {ProcessGTBResponse(xml, wordString);},
+				error: function(jqXHR, textStatus, errorThrown){alert("XML laden mislukt: "+textStatus+" "+errorThrown);}
+			});
+}
+
+// process response from GTB after word search
+function ProcessGTBResponse(xml, word){
+	
+	$("#list_of_senses").empty();
+	$("#list_of_senses").append(
+			$("<span></span>").text("Klik om te tonen")
+			);
+	OpenUrlInFrame("", "");
+	
+	// build select form for choosing a definition
+	var formTagToAdd1 = $("<form></form>")
+		.attr("action", "")
+		.attr("id", "sense_form");
+	var selectTagToAdd1 = $("<select></select>")
+		.attr("id", "selected_sense")
+		.attr("size", "10")
+		.keyup(function(){showOption(this);})
+		.click(function(){showOption(this);});
+	
+	// parse xml with definitions from GTB
+	
+	var nrOfResults = $(xml).find("results").length;
+	
+	if (nrOfResults == 0)
+		{
+		$("#id_query").val("-");
+		OpenUrlInFrame("", "");
+		}
+	
+	if (nrOfResults == 1)
+		{
+		var result = $(xml).find("results").slice(0,1);
+		var wdbIdAndLemma = result.find("id").text();
+		var wdb = wdbIdAndLemma.split(",")[0];
+		var id = wdbIdAndLemma.split(",")[1];		
+		
+		$("#id_query").val(id);
+		OpenUrlInFrame("http://gtb.inl.nl/iWDB/search?actie=article&wdb="+wdb+"&id="+id+"&lemmodern="+word, wdb);
+		
+		}
+
+
+	// if there are more definitions, show no definition in gtb preview by default
+	if (nrOfResults > 1)
+		{
+		OpenUrlInFrame("", "");
+		}
+	
+	// build the selection list
+	
+	$(xml).find("results").each(function(){
+		
+		var wdbIdAndLemma = $(this).find("id").text();
+		var wdb = wdbIdAndLemma.split(",")[0];
+		var id = wdbIdAndLemma.split(",")[1];
+		var modLemma = wdbIdAndLemma.split(",")[2];
+		var sense = removeTags($(this).find("sense").text()).substring(0,60)+"...";
+		
+		selectTagToAdd1.append(
+				$("<option></option>")
+					.attr("value", id+","+wdb+","+modLemma+",http://gtb.inl.nl/iWDB/search?actie=article&wdb="+wdb+"&id="+id+"&lemmodern="+word)
+					.text(sense)
+					.css("width", $("#list_of_senses").css("width"))
+		);
+	});
+	
+	
+	formTagToAdd1.append(selectTagToAdd1);
+	
+	$("#list_of_senses").append(formTagToAdd1);
+	
+	// focus on first option of select list
+	$("#selected_sense").focus();	
+	$("#selected_sense").val($("#selected_sense option:first").val());
+	
+	showOption($("#selected_sense"));
+
+
+
+}
+
+// callback function for keyup- en click-events on senses options 
+// when surfing the list of senses of a word, change the GTB window immediately
+// (that is: show the article straight away)
+function showOption(node){
+	
+	var optionValue = $(node).find(":selected").val().split(",");
+	$("#id_query").val(optionValue[0]);
+	$("#word_query").val(optionValue[2]);
+	OpenUrlInFrame(optionValue[3], optionValue[1]);
+	
+	}
+
+//=======================================================================================
