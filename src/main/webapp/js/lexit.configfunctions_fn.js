6554dfe068dea8b1a0a4e4fb11e55772ba26acac
Mathieu Fannee <mathieu.fannee@inl.nl>
2020-12-16 14:02:47 +0100

    Lex'it - extra fix to fn.getSelectedTextInNode() etc

--- src/main/webapp/js/lexit.configfunctions_fn.js
+++ src/main/webapp/js/lexit.configfunctions_fn.js
@@ -2195,7 +2195,7 @@ fn.getSelectedTextInNode = function(nMixed, sColumnName) {
     var sel, range, priorRange, fulltext = "", text = "";
     
     // all browsers, except IE before version 9
-    if (typeof window.getSelection != "undefined" && window.getSelection.rangeCount > 0) 
+    if (typeof window.getSelection != "undefined" && window.getSelection().rangeCount > 0) 
     {    	
     	range = window.getSelection().getRangeAt(0);        
         priorRange = range.cloneRange();
@@ -2271,7 +2271,7 @@ fn.getWordClickedUponInNode = function(nMixed, sColumnName){
     var sel, range, priorRange, wholeRange, fulltext = "", text = "";
     
     // all browsers, except IE before version 9
-    if (typeof window.getSelection != "undefined" && window.getSelection.rangeCount > 0) 
+    if (typeof window.getSelection != "undefined" && window.getSelection().rangeCount > 0) 
     {    	
         range = window.getSelection().getRangeAt(0);        
         wholeRange = range.cloneRange();
