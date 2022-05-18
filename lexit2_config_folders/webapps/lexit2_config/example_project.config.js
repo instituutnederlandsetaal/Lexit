1c6458fcd2c9c59ca3bcc09c21a8c6bca53e63aa
Mathieu Fannee <mathieu.fannee@inl.nl>
2022-05-18 13:20:01 +0200

    Lex'it - default folders added as examples for new installation

---
+++ lexit2_config_folders/webapps/lexit2_config/example_project.config.js
@@ -0,0 +1,11 @@
+
+// list of tables that must be hidden or visible (don't use both, it's a matter of what's the most convenient)
+oHiddenTablesList = [];
+oShowOnlyTables = [];
+
+// configuration at table level
+oTableSettingsList = {};
+
+// configuration at column level
+oTableConfigurationList = {};
+
